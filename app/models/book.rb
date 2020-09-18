33688bee246189f5315b846af2b52edbe195463c
saito743 <yuka.saito21@gmail.com>
2020-09-18 13:20:47 +0000

    [Update]コメント機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
 	belongs_to :user
+
 	has_many :favorites, dependent: :destroy
+	has_many :comments, dependent: :destroy
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
